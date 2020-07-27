d71ff35265405368653884f12590689575c0ffb1
suzukiserika <soap-bubble@outlook.com>
2020-07-27 13:48:19 +0000

    課題０３

--- practice02.php
+++ practice02.php
@@ -1,31 +1,31 @@
 <?php
 echo 'hello php!';
 
-// 課題１
+// 02課題１
 $a = 3;
 $b = 7;
 echo $a + $b;
 echo "\n";
 
-// 課題２
+// 02課題２
 $array_month = ["1月" , "2月" , "3月" , "4月" , "5月" , "6月" , "7月" , "8月" , "9月" , "10月" , "11月" , "12月"];
 echo $array_month [7];
 echo "\n";
 
-// 課題３
+// 02課題３
 $hello = "Hello. ";
 $name = "Serika";
 $world = "'s World!";
 echo $hello . $name . $world;
 echo "\n";
 
-// 課題４
+// 02課題４
 $tech_boost = "tech";
 $tech_boost .= " boost";
 echo $tech_boost;
 echo "\n";
 
-// 課題５
+// 02課題５
 $calender_2018 = [
 "January" => "1月" , 
 "February" => "2月" , 
@@ -41,3 +41,41 @@ $calender_2018 = [
 "December" => "12月"];
 echo $calender_2018 ["December"];
 echo "\n";
+
+// 03課題１
+$name = "SERIKA";
+if ($name = "SERIKA") {
+    echo "私は SERIKA です";
+}
+else {
+    echo"あなたの名前ではありません";
+}
+echo "\n";
+
+// 03課題２
+$total = 0;
+echo $total;
+echo "\n";
+
+for ($i =  0; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
+echo "\n";
+
+// 03課題３
+$fruits = array("apple" , "strawberry" , "orange" , "lemon" , "grape");
+foreach($fruits as $fruits){
+    echo "要素は" . $fruits;
+    echo "\n";
+}
+echo "\n";
+
+// 03課題４
+for ($i = 0; $i < 100; $i++){
+    if ($i % 5 == 0){
+    echo $i;
+    }
+}
+echo "\n";
+
